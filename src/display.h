9e8370a79b5cb5047edaa10a35f54fbafa0567f5
John-Patacsil <patacsiljohn382@gmail.com>
2024-03-07 12:04:25 +0000

    First Commit of "Cheese Run" code

---
+++ src/display.h
@@ -0,0 +1,30 @@
+void display_begin(void);
+void delay(uint32_t dly);
+void fillRectangle(uint16_t x,uint16_t y,uint16_t width, uint16_t height, uint16_t colour);
+void putPixel(uint16_t x, uint16_t y, uint16_t colour);
+void putImage(uint16_t x, uint16_t y, uint16_t width, uint16_t height, const uint16_t *Image, int hOrientation,int vOrientation);
+void drawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t Colour);
+void drawRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t Colour);
+void drawCircle(uint16_t x0, uint16_t y0, uint16_t radius, uint16_t Colour);
+void fillCircle(uint16_t x0, uint16_t y0, uint16_t radius, uint16_t Colour);
+void printText(const char *Text,uint16_t x, uint16_t y, uint16_t ForeColour, uint16_t BackColour);
+void printTextX2(const char *Text, uint16_t x, uint16_t y, uint16_t ForeColour, uint16_t BackColour);
+void printNumber(uint16_t Number, uint16_t x, uint16_t y, uint16_t ForeColour, uint16_t BackColour);
+void printNumberX2(uint16_t Number, uint16_t x, uint16_t y, uint16_t ForeColour, uint16_t BackColour);
+uint16_t RGBToWord(uint16_t R, uint16_t G, uint16_t B);
+
+// Functions to set display for each level
+int maze_level1(int);
+void maze_level2(void); 
+void maze_level3(void);
+
+// These functions pass oldx, oldy, and the level number and return the new level number
+int level1_pass(uint16_t,uint16_t,int); // Function for when the player passes level 1
+int level2_pass(uint16_t,uint16_t,int); // Function for when the player passes level 2
+
+void level3_pass(uint16_t,uint16_t); // Function for when the player passes final level
+
+// Functions for border collisions for maze walls
+int touchingVline(int,int,int,int,int,int,int);
+int touchingHline(int,int,int,int,int,int,int);
+
---
+++ src/hitmaze.c
@@ -0,0 +1,26 @@
+//SOME NOTES FROM CLASS REGARDING MAZE SETUP AND PROCESSING
+
+int touchingVLine(int lx, int ly, int lh, int px, int py, int pw, int ph)
+{
+    int returnvalue = 0;
+    if ((lx >= px) || (lx <= (px + pw)))
+    {
+        if ((py >= ly) && (py <= (ly + lh)))
+            returnvalue = 1;
+        if (((py + ph) >= ly) && ((py + ph) <= (ly + lh)))
+            returnvalue = 1;
+    }
+    return returnvalue;
+}
+int touchingHLine(int lx, int ly, int lw, int px, int py, int pw, int ph)
+{
+    int returnvalue = 0;
+    if ((ly >= py) || (ly <= (py + ph)))
+    {
+        if ((px >= lx) && (px <= (lx + lw)))
+            returnvalue = 1;
+        if (((py + pw) >= lx) && ((pw + pw) <= (lx + lw)))
+            returnvalue = 1;
+    }
+    return returnvalue;
+}
